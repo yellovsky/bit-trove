8879247b990424d4c9a7a470439bd80b92f35b19
Dmitry Elovsky <dmitry.elovsky@gmail.com>
2025-07-23 20:33:49 +0500

    refactor: enhance Typography component structure and naming conventions
    - Refactored Typography component to improve organization and readability by introducing named constants for display names.
    - Updated Blockquote, Code, Kbd, Paragraph, Heading, OrderedList, UnorderedList, ListItem, and HorizontalRule components to follow a consistent structure.
    - Enhanced export statements to include all typography-related components and their respective props for better accessibility and usability.

--- packages/ui/src/components/Typography.tsx
+++ packages/ui/src/components/Typography.tsx
@@ -6,27 +6,62 @@ import { Link as RouterLink } from 'react-router';
 import { useEnhanceTo } from '@repo/ui/hooks/enhance-to';
 import { cn } from '@repo/ui/lib/utils';
 
-export const Blockquote: FC<ComponentProps<'blockquote'>> = ({ className, ...props }) => (
+/* -------------------------------------------------------------------------------------------------
+ * Blockquote
+ * -----------------------------------------------------------------------------------------------*/
+const NAME_BLOCKQUOTE = 'Blockquote';
+
+type BlockquoteProps = ComponentProps<'blockquote'>;
+
+const Blockquote: FC<BlockquoteProps> = ({ className, ...props }) => (
   <blockquote {...props} className={cn('typography-blockquote', className)} />
 );
-Blockquote.displayName = 'Blockquote';
 
-export const Code: FC<ComponentProps<'code'>> = ({ className, ...props }) => (
+Blockquote.displayName = NAME_BLOCKQUOTE;
+
+/* -------------------------------------------------------------------------------------------------
+ * Code
+ * -----------------------------------------------------------------------------------------------*/
+const NAME_CODE = 'Code';
+
+type CodeProps = ComponentProps<'code'>;
+
+const Code: FC<CodeProps> = ({ className, ...props }) => (
   <code {...props} className={cn('typography-code', className)} />
 );
-Code.displayName = 'Code';
 
-export const Kbd: FC<ComponentProps<'kbd'>> = ({ className, ...props }) => (
-  <kbd {...props} className={cn('typography-kbd', className)} />
-);
-Kbd.displayName = 'Kbd';
+Code.displayName = NAME_CODE;
 
-export const Paragraph: FC<ComponentProps<'p'>> = ({ className, ...rest }) => (
+/* -------------------------------------------------------------------------------------------------
+ * Kbd
+ * -----------------------------------------------------------------------------------------------*/
+const NAME_KBD = 'Kbd';
+
+type KbdProps = ComponentProps<'kbd'>;
+
+const Kbd: FC<KbdProps> = ({ className, ...props }) => <kbd {...props} className={cn('typography-kbd', className)} />;
+
+Kbd.displayName = NAME_KBD;
+
+/* -------------------------------------------------------------------------------------------------
+ * Paragraph
+ * -----------------------------------------------------------------------------------------------*/
+const NAME_PARAGRAPH = 'Paragraph';
+
+type ParagraphProps = ComponentProps<'p'>;
+
+const Paragraph: FC<ParagraphProps> = ({ className, ...rest }) => (
   <p {...rest} className={cn('typography-paragraph', className)} />
 );
-Paragraph.displayName = 'Paragraph';
 
-export const headingVariants = cva('typography-heading', {
+Paragraph.displayName = NAME_PARAGRAPH;
+
+/* -------------------------------------------------------------------------------------------------
+ * Heading
+ * -----------------------------------------------------------------------------------------------*/
+const NAME_HEADING = 'Heading';
+
+const headingVariants = cva('typography-heading', {
   variants: {
     size: {
       h1: 'typography-heading-h1',
@@ -39,38 +74,70 @@ export const headingVariants = cva('typography-heading', {
 type HeadingProps = ComponentProps<'h1'> &
   VariantProps<typeof headingVariants> & { asChild?: boolean; order: 1 | 2 | 3 };
 
-export const Heading: FC<HeadingProps> = ({ order, size, asChild = false, ...props }) => {
+const Heading: FC<HeadingProps> = ({ order, size, asChild = false, ...props }) => {
   const Comp = asChild ? Slot.Root : `h${order}`;
   const derivedSize: Exclude<VariantProps<typeof headingVariants>['size'], undefined> = size || `h${order}`;
 
   return <Comp {...props} className={cn(headingVariants({ size: derivedSize }), props.className)} />;
 };
-Heading.displayName = 'Heading';
+Heading.displayName = NAME_HEADING;
 
-export const OrderedList: FC<ComponentProps<'ol'>> = ({ className, ...props }) => (
+/* -------------------------------------------------------------------------------------------------
+ * OrderedList
+ * -----------------------------------------------------------------------------------------------*/
+const NAME_ORDERED_LIST = 'OrderedList';
+
+type OrderedListProps = ComponentProps<'ol'>;
+
+const OrderedList: FC<OrderedListProps> = ({ className, ...props }) => (
   <ol {...props} className={cn('typography-list', className)} />
 );
-OrderedList.displayName = 'OrderedList';
 
-export const UnorderedList: FC<ComponentProps<'ul'>> = ({ className, ...props }) => (
+OrderedList.displayName = NAME_ORDERED_LIST;
+
+/* -------------------------------------------------------------------------------------------------
+ * UnorderedList
+ * -----------------------------------------------------------------------------------------------*/
+const NAME_UNORDERED_LIST = 'UnorderedList';
+
+type UnorderedListProps = ComponentProps<'ul'>;
+
+const UnorderedList: FC<UnorderedListProps> = ({ className, ...props }) => (
   <ul {...props} className={cn('typography-list', className)} />
 );
-UnorderedList.displayName = 'UnorderedList';
 
-export const ListItem: FC<ComponentProps<'li'>> = ({ children, ...props }) => (
+UnorderedList.displayName = NAME_UNORDERED_LIST;
+
+/* -------------------------------------------------------------------------------------------------
+ * ListItem
+ * -----------------------------------------------------------------------------------------------*/
+const NAME_LIST_ITEM = 'ListItem';
+
+type ListItemProps = ComponentProps<'li'>;
+
+const ListItem: FC<ListItemProps> = ({ children, ...props }) => (
   <li {...props}>
     <div className="inline-flex">{children}</div>
   </li>
 );
-ListItem.displayName = 'ListItem';
 
-export const HorizontalRule: FC<ComponentProps<'hr'>> = ({ className, ...props }) => (
-  <hr {...props} className={cn('typography-horizontal-rule', className)} />
-);
-HorizontalRule.displayName = 'HorizontalRule';
+ListItem.displayName = NAME_LIST_ITEM;
 
 /* -------------------------------------------------------------------------------------------------
- * Button
+ * HorizontalRule
+ * -----------------------------------------------------------------------------------------------*/
+const NAME_HORIZONTAL_RULE = 'HorizontalRule';
+
+type HorizontalRuleProps = ComponentProps<'hr'>;
+
+const HorizontalRule: FC<HorizontalRuleProps> = ({ className, ...props }) => (
+  <hr {...props} className={cn('typography-horizontal-rule', className)} />
+);
+
+HorizontalRule.displayName = NAME_HORIZONTAL_RULE;
+
+/* -------------------------------------------------------------------------------------------------
+ * TextLink
  * -----------------------------------------------------------------------------------------------*/
 const TEXT_LINK_NAME = 'TextLink';
 
@@ -109,6 +176,17 @@ TextLink.displayName = TEXT_LINK_NAME;
 
 /* -----------------------------------------------------------------------------------------------*/
 
-export { TextLink };
+export { TextLink, Blockquote, Code, Kbd, Paragraph, Heading, OrderedList, UnorderedList, ListItem, HorizontalRule };
 
-export type { TextLinkProps };
+export type {
+  TextLinkProps,
+  BlockquoteProps,
+  CodeProps,
+  KbdProps,
+  ParagraphProps,
+  HeadingProps,
+  OrderedListProps,
+  UnorderedListProps,
+  ListItemProps,
+  HorizontalRuleProps,
+};
