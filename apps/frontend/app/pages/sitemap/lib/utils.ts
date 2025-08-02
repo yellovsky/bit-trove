eb360209dc74aabf86d45a9e811560df129303e1
Dmitry Elovsky <dmitry.elovsky@gmail.com>
2025-08-03 00:11:56 +0500

    fix: format date output in getLastMod utility for consistency
    - Updated the getLastMod function to pad month and day values with leading zeros, ensuring a consistent date format (YYYY-MM-DD) across the application.

--- apps/frontend/app/pages/sitemap/lib/utils.ts
+++ apps/frontend/app/pages/sitemap/lib/utils.ts
@@ -1,5 +1,5 @@
 export const getLastMod = (maybeDate: Date | number): string => {
   const date = new Date(maybeDate);
 
-  return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
+  return `${date.getFullYear()}-${(date.getMonth() + 1).toString().padStart(2, '0')}-${date.getDate().toString().padStart(2, '0')}`;
 };
