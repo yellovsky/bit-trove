e1472bd1ca3c0bbc731007c1406b130a69897169
Dmitry Elovsky <dmitry.elovsky@gmail.com>
2025-08-03 11:52:51 +0500

    feat: add XML declaration to sitemap and URL set output
    - Included XML declaration (`<?xml version="1.0" encoding="UTF-8"?>`) in the toString method of SitemapIndexNode and UrlSetNodes classes to ensure proper XML formatting for sitemap generation.

--- apps/frontend/app/pages/sitemap/model/sitemapindex-node.ts
+++ apps/frontend/app/pages/sitemap/model/sitemapindex-node.ts
@@ -9,6 +9,7 @@ export class SitemapIndexNode {
 
   toString() {
     return `
+		  <?xml version="1.0" encoding="UTF-8"?>
 			<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 				${this.sitemaps.map((node) => node.toString()).join('')}
 			</sitemapindex>
